43a52de7fd198d0fbac6afa3b00c98cbf4f465f1
Edmon <edmon.grigoryan.2005@gmail.com>
2025-09-26 18:56:20 +0300

    fff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Items from './components/Items'
 
-function App() {
+class App extends React.Component() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул',
+          img: 'chair.jpg',
+          desc: 'df;j womv orjmgk spromg,k vokmtr[o',
+          category: 'chairs',
+          price: '499'
+        },
+
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'df;j womv orjmgk spromg,k vokmtr[o',
+          category: 'tables',
+          price: '3199'
+        },
+
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpg',
+          desc: 'df;j womv orjmgk spromg,k vokmtr[o',
+          category: 'sofas',
+          price: '9999'
+        }
+      ]
+    }
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper"> 
+    <Header/>
+    <Items items = {this.state.items}/>
+    <Footer/>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <ul className='nav'>
+                <li>Про нас</li>
+                <li>Контакты</li>
+                <li>Кабинет</li>
+            </ul>
+            <span className = 'logo'> House Staff </span>
+        </div>
+        <div className='presentation'>
+            
+        </div>
+    </header>
+  )
+}
